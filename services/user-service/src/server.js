e0a1b596ab1b0e3e31e5a4f136231f66c1512516
chirag <shettychirag16@gmail.com>
2026-04-11 18:29:06 +0000

    Service Update

--- services/user-service/src/server.js
+++ services/user-service/src/server.js
@@ -9,22 +9,7 @@ const app = express();
 
 // User service CAN parse JSON because it's the final destination
 app.use(express.json());
-const allowedOrigins = [
-  'http://localhost:5173', 
-  'https://hire-flow-gules.vercel.app' // Add your production frontend here
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 connectDB();
 // Mount the routes so they match the Gateway's incoming request exactly
 app.use("/api/auth", authRoutes);
